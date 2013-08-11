fefc14a5517819fe164c748bf2e1187bdc72c7d2
David Kua <david@kua.io>
2013-08-11 10:49:13 -0400

    Updated website and added intro

--- info.py
+++ info.py
@@ -1,4 +1,8 @@
-print "Name: David Kua"
-print "Site: www.davidkua.com"
-print "Email: david@davidkua.com"
-print "Twitter: @davidkua"
+print ("Name: David Kua")
+print ("Site: david.kua.io")
+print ("Email: david@kua.io")
+print ("Twitter: @davidkua")
+print ("Github: dkua")
+
+message = "Nice to meet you! I'm currently an intern developer at VM Farms and CS student at UofT. Feel free to contact me at any of the above for anything!"
+print(message)
